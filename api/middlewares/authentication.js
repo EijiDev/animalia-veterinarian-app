e4bd6e73dad1a42df850f7a8206223fa2db9d57a
Esteban Perafan <bestebanperafan@gmail.com>
2023-11-13 07:43:03 -0300

    Cosas

--- api/middlewares/authentication.js
+++
@@ -1,8 +0,0 @@
-function ensureAuth(req, res, next){
-    let token = req.get('authorization');
-    console.log('Token ' + token);
-    token = parseInt(token);
-    next();
-}
-
-module.exports = ensureAuth;
--- api/routes/login.js
+++
@@ -1,8 +0,0 @@
-const loginRouter = require('express').Router();
-const LoginController = require('../controllers/login');
-
-loginRouter.post('/login', LoginController.logInUser);
-
-loginRouter.post('/register', LoginController.registerUser);
-
-module.exports = loginRouter;
